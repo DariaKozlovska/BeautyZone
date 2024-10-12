bcc81e9da9780870a851f93d2c02f15d5d4a4a8f
DariaKozlovska <odriy.lovska@gmail.com>
2024-10-12 12:30:26 +0200

    adaption

---
+++ js/mobile-menu.js
@@ -0,0 +1,16 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBtnOpen = document.querySelector(".menu-btn-open");
+const menuBtnClose = document.querySelector(".menu-btn-close");
+
+const toggleModalMobile = () => mobileMenu.classList.toggle("is-open");
+
+menuBtnOpen.addEventListener("click", toggleModalMobile);
+menuBtnClose.addEventListener("click", toggleModalMobile);
+
+const modal = document.querySelector(".backdrop");
+const modalBtnOpen = document.querySelector(".modal-btn-open");
+const modalBtnClose = document.querySelector(".modal-btn-close");
+
+const toggleModal = () => modal.classList.toggle("is-hidden");
+modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnClose.addEventListener("click", toggleModal);
